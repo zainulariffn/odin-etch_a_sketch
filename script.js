23ad997832b2d8870971985fc5144f4d70ef200c
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2024-11-27 01:04:51 +0800

    Generate random colour

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const cntr = document.querySelector(".container");
+const btn = document.querySelector(".btn");
 
-function createGrid() {
+const createGrid = () => {
   for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("grid-container");
 
+    div.addEventListener("mouseover", () => {
+      const random = genRandomColor();
+      div.style.backgroundColor = random;
+    });
     cntr.append(div);
   }
-}
+};
 createGrid();
+
+const genRandomColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
