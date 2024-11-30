602187c9268e55de366e4d100f994175d7e7e929
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2024-12-01 01:36:07 +0800

    Add reset button

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const cntr = document.querySelector(".container");
+const cntnr = document.querySelector(".container");
 const btn = document.querySelector(".btn");
+const reset = document.querySelector(".reset");
 
 const createGrid = () => {
+  cntnr.textContent = "";
+
   for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("grid-container");
@@ -10,7 +13,7 @@ const createGrid = () => {
       const random = genRandomColor();
       div.style.backgroundColor = random;
     });
-    cntr.append(div);
+    cntnr.append(div);
   }
 };
 createGrid();
@@ -20,4 +23,17 @@ const genRandomColor = () => {
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
   return `rgb(${r}, ${g}, ${b})`;
+};
+
+btn.onclick = () => {
+  let answer = prompt(`Please enter grid size to be played: no more than 100`);
+  if (isNaN(answer) || answer == 0 || answer >= 100 || answer == "") {
+    alert(`Please enter a valid number`);
+    return;
+  }
+  createGrid();
+};
+
+reset.onclick = () => {
+  createGrid();
 };
